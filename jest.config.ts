fa0d7256a0217922fbd709954bd303dcfd9815a6
Krealer <Krealerkimizuki@gmail.com>
2025-06-13 13:57:53 +0100

    chore: set up jest with ts

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import { Config } from 'jest';
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig: Config = {
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFilesAfterEnv: [],
+};
+
+export default createJestConfig(customJestConfig);
